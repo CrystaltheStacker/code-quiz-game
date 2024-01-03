1a33d55ebee2ec8b99e2fa2e6c42bdf1ceca417b
Crystal Hikal <crystaluncwebdev@gmail.com>
2024-01-03 15:02:51 -0500

    Changes to JS for correct/wrong function, modified README and addtion of codesnaps and screenrecording

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,78 +1,65 @@
-// DECLARATIONS
-
-// VAR FOR TIMER   (global) 
 let time = document.querySelector(".timer");
 let score = document.querySelector("#score");
 let secondsLeft = 75;
 
-//VAR FOR BUTTONS (global )
 const start = document.querySelector("#start");
 
-// VAR FOR INTRO/START
 const gameIntro = document.querySelector("#game-begins");
 
-//CALL END LOAD ELEMENT VAR
 let questionsArr = document.querySelector(".all-questions");
 
-// ELEMENT LOCATIONS VAR
 let questionElem = document.querySelector("#question");
 const correctWrong = document.querySelector("#right-wrong");
 let questionCount = 0;
 
-// FINAL SCORE VAR
 const finalElem = document.querySelector("#final-score");
 let initialsInput = document.querySelector("#initials");
 
-// HIGHSCORE VAR 
 const highScoresElem = document.querySelector("#high-scores");
 let scoreListElem = document.querySelector(".score-list");
 let scoreList = [];
 
-// CALL OUT THE ANSWER CLASS BUTTON
 const ansBtn = document.querySelectorAll("button.answer-btn");
 
-// VAR SUBMITINT, GO, CLEAR, VIEW
 let submitScrBtn = document.querySelector("#submit-score");
 let clearScrBtn = document.querySelector("#clearScores");
 let viewScrBtn = document.querySelector("#view-scores");
 let goBackBtn = document.querySelector("#goBack");
 
-// VAR ANSWER CALL
 const ans1Btn = document.querySelector("#answer-1");
 const ans2Btn = document.querySelector("#answer-2");
 const ans3Btn = document.querySelector("#answer-3");
 const ans4Btn = document.querySelector("#answer-4");
 
-// ARRAY OF FIVE QUESTIONS
 const questions = [ 
     {
         question: "Commonly used data types DO not use __________.",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctAnswer: "3"
+        correctAnswer: 2
     },
     {
         question: "The condition in an if / else statement is enclosed with ___________.",
         answers: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-        correctAnswer: "3"
+        correctAnswer: 2
     },
     {
         question: "Arrays in JavaScript can be used to store ____________.",
-        answers: ["numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-        correctAnswer: "4"
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: 3
     },
     {
         question: "String values must be enclosed within ____________ when being assigned to variables.",
         answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-        correctAnswer: "3"
+        correctAnswer: 2
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-        correctAnswer: "4"
+        correctAnswer: 3
     }
 ];
 
-// TIMER FUNCTION STARTS PROCESS 
+
 const setTime = () => {
     let timerInterval = setInterval(() => {
         secondsLeft--;
@@ -87,7 +74,7 @@ const setTime = () => {
     }, 1000);
 };
 
-// QUIZ BEGIN FUNCTION
+
 const startQuiz = () => {
     gameIntro.style.display = "none";
     questionsArr.style.display = "block";
@@ -97,7 +84,7 @@ const startQuiz = () => {
     setQuestion(questionCount);
 };
 
-// SET QUESTION FUNCTION
+
 const setQuestion = (id) => {
     if (id < questions.length) {
         questionElem.textContent = questions[id].question;
@@ -108,31 +95,27 @@ const setQuestion = (id) => {
     }
 };
 
-// EVENT FUNCTION CHECK ANSWERS BEGIN PROCESS
+
 const checkAnswer = (event) => {
     event.preventDefault();
 
-    // CREATING ELEMENT OF RIGHT OR WRONG
     correctWrong.style.display = "block";
     let p = document.createElement("p");
     correctWrong.appendChild(p);
 
-    // DISPLAY NEW ELEMENT FOR X AMOUNT OF TIME
     setTimeout(() => {
-        p.style.display = 'none';
+    p.style.display = 'none';
     }, 1000);
 
-    // RIGHT OR WRONG ANSWER CONDITIONAL STATEMENTS CORRECT
-    if (questions[questionCount].correctAnswer === event.target.value) {
-        p.textContent = "Correct!";
-    } else if (questions[questionCount].correctAnswer !== event.target.value) {
-        secondsLeft = secondsLeft - 10;
-        p.textContent = "Wrong!";
+    if (parseInt(questions[questionCount].correctAnswer) === parseInt(event.target.value)) {
+    p.textContent = "Correct!";
+    } else {
+    secondsLeft = secondsLeft - 10;
+    p.textContent = "Wrong!";
     }
 
-    // CYCLE 
     if (questionCount < questions.length) {
-        questionCount++;
+    questionCount++;
     }
     setQuestion(questionCount);
 };
@@ -146,7 +129,7 @@ const addScore = (event) => {
     let init = initialsInput.value.toUpperCase();
     scoreList.push({ initials: init, score: secondsLeft });
 
-    // HIGH SCORE SORTING LIST
+
     scoreList = scoreList.sort((a, b) => (a.score < b.score ? 1 : -1));
 
     scoreListElem.innerHTML = "";
@@ -156,7 +139,7 @@ const addScore = (event) => {
         scoreListElem.append(li);
     }
 
-    // STORAGE OF SCORE 
+
     storeScores();
     displayScores();
 };
@@ -166,34 +149,32 @@ const storeScores = () => {
 };
 
 const displayScores = () => {
-    // Parsing the JSON string to an object
+
     let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
 
-    // WHEN RETRIEVED FROM LOCAL, ARRAY
     if (storedScoreList !== null) {
         scoreList = storedScoreList;
     }
 };
 
-// CLEAR THE SCORE
+
 const clearScores = () => {
     localStorage.clear();
     scoreListElem.innerHTML = "";
 };
 
-// START OFF ALL EVENT 
-// Start timer and display the first question when click start quiz
+
 start.addEventListener("click", startQuiz);
 
-// CHECK ANSWER LISTENER EVENT
+
 ansBtn.forEach(item => {
     item.addEventListener('click', checkAnswer);
 });
 
-// ADDING A SCORE EVENT
+
 submitScrBtn.addEventListener("click", addScore);
 
-// GO BACK LISTENER EVENT FUNCTION 
+
 goBackBtn.addEventListener("click", () => {
     highScoresElem.style.display = "none";
     gameIntro.style.display = "block";
@@ -201,10 +182,10 @@ goBackBtn.addEventListener("click", () => {
     time.textContent = `Time:${secondsLeft}s`;
 });
 
-// CLEAR SCORE
+
 clearScrBtn.addEventListener("click", clearScores);
 
-// HIGH SCORE BUTTON ALERT AND DISPLAY LISTENER EVENT
+
 
 
 
--- README.md
+++ README.md
@@ -1,99 +1,66 @@
 # 04 Web APIs: Code Quiz
 
-## Your Task
+# code-quiz-game
 
-At some point in your journey to become a full-stack web developer, you'll likely be asked to complete a coding assessment&mdash;perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges. 
+## My Task
 
-To help familiarize you with these tests and allow you to use the skills covered in this module, this Challenge invites you to build a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean, polished, and responsive user interface. 
+As a junior developer, I was tasked with the creation of a code quiz game.
 
-This week's coursework will equip you with all the skills you need to succeed in this assignment.
+## Website Link
+![GitHub Website Link]("https://github.com/CrystaltheStacker/code-quiz-game")
 
-## User Story
+## Link to final deployed landing page
+![Final Landing Page]("https://crystalthestacker.github.io/code-quiz-game/")
 
-```
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-```
+## Contact
+crystaluncwebdev@gmail.com
 
-## Acceptance Criteria
+## Screenshot of HTML Code
+![Alt text](Assets/HTML%20screenshot.png)
 
-```
-GIVEN I am taking a code quiz
-WHEN I click the start button
-THEN a timer starts and I am presented with a question
-WHEN I answer a question
-THEN I am presented with another question
-WHEN I answer a question incorrectly
-THEN time is subtracted from the clock
-WHEN all questions are answered or the timer reaches 0
-THEN the game is over
-WHEN the game is over
-THEN I can save my initials and my score
-```
+## Screenshot of CSS Code
+![Alt text](Assets/CSS%20screenshot.png)
 
-## Mock-Up
+## Screenshot of JS Code
+![Alt text](Assets/JS%20screenshot.png)
 
-The following animation demonstrates the application functionality:
+## Video Demo of my Random Password Generator
+<video src="/Assets/quiz-code-game-screenrecord.mp4" controls title="Screenrecord Quiz Game"></video>
 
-![A user clicks through an interactive coding quiz, then enters initials to save the high score before resetting and starting over.](./Assets/04-web-apis-homework-demo.gif)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as "0", it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the preceding acceptance criteria.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
+## Acceptance Criteria 
+* When the user wants to play the quiz game, they are presented with a set of multiple choice questions and can compete to be the highest scored player. 
+* They will be limited on time and their score/time will decrease if they answer incorrectly.
+* The game runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It has a clean, polished, and responsive user interface. 
+* Final landing page deployed at live URL.
+* Final landing page loads with no errors.
+* Submitted with GitHub URL.
 * Repository has a unique name.
-
 * Repository follows best practices for file structure and naming conventions.
+* Repository follows best practices for naming conventions, indentation, etc.
+* Repository final code does not contain multiple descriptive commit messages because as techies we all know what the code means. 
+* Repository contains quality README file with description, screenshot, and link to deployed application.
 
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
----
 
 © 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+MIT License
+
+Copyright (c) 2023 Crystal Hikal
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
